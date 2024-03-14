6102a951278867ca26edc5c695f86e5d59571a39
Ashlina-pj <ashlina.pj@indiumsoft.com>
2024-03-14 18:50:35 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { ReceipeDetailComponent } from './receipes/receipe-detail/receipe-detail.component';
+import { ReceipeListComponent } from './receipes/receipe-list/receipe-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ReceipeItemComponent } from './receipes/receipe-list/receipe-item/receipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReceipesComponent,
+    ReceipeDetailComponent,
+    ReceipeListComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    ReceipeItemComponent
   ],
   imports: [
     BrowserModule,
